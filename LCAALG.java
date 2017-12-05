7ab41ea83afe2737d6d5c91763e00736f9213449
wang16108 wang16108 <wangweiching@gmail.com>
2017-12-05 11:14:08 -0800

    second commit

--- LCAALG.java
+++ LCAALG.java
@@ -3,71 +3,33 @@ import java.util.List;
 
 public class LCAALG {
 	
-	private List<Integer> l1 = new ArrayList<Integer>();
-	private List<Integer> l2 = new ArrayList<Integer>();
+	private static List<Integer> l1 = new ArrayList<Integer>();
+	private static List<Integer> l2 = new ArrayList<Integer>();
 	
-	boolean findPath(Node root, int n, List<Integer> path) {
-		
-
-		System.out.println("adding: "+root.val);
-//		//add to the traversed list
+	boolean findPath(Node root, int n, List<Integer> path ) {
 		path.add(root.val);
-		System.out.println("after adding: "+path);
-		//exit from findPath if match
-		if(root.val==n) {
+		if(root.val== n) {
 			return true;
 		}
+		System.out.println(root.val);
 		
-		//recusively look into the left child
-		if(root.left!=null && findPath(root.left,n,path)) {
+		
+		if(root.left!=null && findPath(root.left,n,l1)) {
+			System.out.println("done with reading left child");
 			return true;
 		}
-		//recursively look into right child
-		if(root.right!=null && findPath(root.right,n,path)) {
+		if(root.right!=null && findPath(root.right,n,l2)) {
+			System.out.println("done with reading right child");
 			return true;
 		}
-		System.out.println("current content: "+path);
-		System.out.println("will remove "+path.get(path.size()-1));
+		System.out.println("Content of path: "+path);
 		path.remove(path.size()-1);
-		System.out.println("after removal: "+path);
 		
 		return false;
 	}
-	int findLCA(Node root, int n1, int n2) {
-		l1.clear();
-		l2.clear();
-		
-		
-		//use findLCAInternal function to find path path for each node
-		return findLCAForNodes(root, n1, n2);
-		
-	}
-	int findLCAForNodes(Node root, int n1, int n2) {
-		boolean res;
-		//keep adding to path 1
-		do {
-			res=findPath(root,n1,l1);
-		}while(!res);
-		
-		//keep adding to path 2
-		do {
-			res=findPath(root,n2,l2);
-		}while(!res);
-
-		int i;
-		//break out if path differs
-		for(i=0;i<l1.size()&&i<l2.size();i++) {
-			System.out.print("at index "+i+"="+l1.get(i)+" "+l2.get(i));
-			if(l1.get(i)!=l2.get(i)) {
-				break;
-			}
-		}
-		
-		return l1.get(i-1);
-		
-	}
 	public static void main(String[] args) {
-		LCAALG lcaalg = new LCAALG();
+		
+		LCAALG lca = new LCAALG();
 		
 		Node n = new Node(1);
 
@@ -82,10 +44,25 @@ public class LCAALG {
 		n.right.left = new Node(6);
 		
 		n.right.right = new Node(7);
-		
-		System.out.println("LCA(6,7):" + lcaalg.findLCA(n, 6,7));
+
+		l1.clear();
+		l2.clear();
+
+		System.out.println(l1);
+		System.out.println(l2);
+		int i;
+		boolean res;
+		//keep adding to path 2
+		do {
+			res=lca.findPath(n,5,l2);
+		}while(!res);
+
+		System.out.println(l1);
+		System.out.println(l2);
+
+//		}
+//		System.out.println("LCA is: "+l1.get(i-1));
 		
 	}
-	
-	
+
 }
